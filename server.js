0d3b521c3cd3dc73b33b54a97747ae2bb48f6450
Stanlo06-arch <Stanlo06@web.de>
2026-09-15 22:25:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/health', (req, res) => {
   });
 });
 
-app.use('/api/auth', require('./routes/auth'));
+app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/members', require('./routes/members'));
 app.use('/api/orders', require('./routes/orders'));
 app.use('/api/appointments', require('./routes/appointments'));
@@ -37,6 +37,10 @@ initDb()
     });
   })
   .catch((err) => {
-    console.error('Datenbank-Initialisierung fehlgeschlagen:', err);
+    console.error(
+      'Datenbank-Initialisierung fehlgeschlagen:',
+      err
+    );
+
     process.exit(1);
   });
